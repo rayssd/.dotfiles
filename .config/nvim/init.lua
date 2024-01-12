f233d734047f99e29fe78058ba766436f26d611a
rayssd <67847901+rayssd@users.noreply.github.com>
2024-01-12 15:25:51 +1100

    Test GPG

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.loaded_python3_provider = 0
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
  if not vim.loop.fs_stat(lazypath) then
    vim.fn.system({
@@ -11,8 +14,6 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
  end
 vim.opt.runtimepath:prepend(lazypath)
 
-vim.g.loaded_python3_provider = 0
-vim.g.mapleader = " "
 
 if vim.g.vscode then
 
